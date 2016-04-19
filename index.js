19e42dd9a91410e44922875e2b20c3c71579b187
Michael Toth <tofudrummer@gmail.com>
2016-04-18 17:25:06 -0700

    Zordon now can be found in a room and responds

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ var secrets = require("./secrets");
 
 var mybot = new Discord.Client();
 
+// Utility function to find the bot among list of users
+// bot name should be specified in secrets file
+function findZordon(users) {
+  return users.filter(function(user) {
+    return user.username === 'zordon'
+  });
+}
+
 mybot.on("message", function(message){
-    if(message.content === "ping")
-        mybot.reply(message, "pong");
+  var zordon = findZordon(mybot.users)[0]; // result returned as an array. First one always correct
+  // error if more than one in the array
+  if(message.isMentioned(zordon)) 
+      mybot.reply(message, "What is it, ranger?");
 });
 
 mybot.login(secrets.email, secrets.password);
